94ade01febe24ad4dfec08dc79286e62aa2041f5
Justin Tadlock <justin@justintadlock.com>
2014-10-24 15:00:57 -0500

    Clean up admin.php.  Remove old CSS no longer needed for the admin.  Load admin menus with proper labels, caps, and URLs.

--- admin/admin.php
+++ admin/admin.php
@@ -1,42 +1,43 @@
 <?php
 
+/* Add admin menu items. */
 add_action( 'admin_menu', 'mb_admin_menu' );
 
+/**
+ * Adds admin menu items needed by the plugin.  Rather than having multiple top-level menu items 
+ * like some plugins, which shall remain unnamed, we'll consolidate everything into a single 
+ * item.  Yay for no clutter!
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ */
 function mb_admin_menu() {
 
-	remove_submenu_page( 'edit.php?post_type=forum_topic', 'edit.php?post_type=forum_topic' );
-	//remove_submenu_page( 'edit.php?post_type=forum_topic', 'edit-tags.php?taxonomy=forum&post_type=forum_topic' );
-	//remove_submenu_page( 'edit.php?post_type=forum_topic', 'edit-tags.php?taxonomy=forum_tag&post_type=forum_topic' );
+	/* Get post type names. */
+	$forum_type   = mb_get_forum_post_type();
+	$topic_type   = mb_get_topic_post_type();
+	$reply_type   = mb_get_reply_post_type();
 
-	//add_menu_page( __( 'Message Board', 'message-board' ), __( 'Forums', 'message-board' ), 'manage_forums', 'edit-tags.php?taxonomy=forum&post_type=forum_topic', '', 'dashicons-format-chat', '42' );
+	/* Get post type objects. */
+	$topic_object = get_post_type_object( $topic_type );
+	$reply_object = get_post_type_object( $reply_type );
 
-	add_submenu_page( 'edit.php?post_type=forum', __( 'Topics',  'message-board' ), __( 'Topics',  'message-board' ), 'edit_forum_topics',  'edit.php?post_type=forum_topic' );
-	add_submenu_page( 'edit.php?post_type=forum', __( 'Replies', 'message-board' ), __( 'Replies', 'message-board' ), 'edit_forum_replies', 'edit.php?post_type=forum_reply' );
-	add_submenu_page( 'edit.php?post_type=forum', __( 'Tags',    'message-board' ), __( 'Tags',    'message-board' ), 'manage_forums',      'edit-tags.php?taxonomy=forum_tag&post_type=forum_topic' );
+	/* Add the topic menu page. */
+	add_submenu_page( 
+		"edit.php?post_type={$forum_type}", 
+		$topic_object->labels->all_items, 
+		$topic_object->labels->all_items, 
+		$topic_object->cap->edit_posts, 
+		"edit.php?post_type={$topic_type}" 
+	);
 
-/*
-
-http://localhost/wp-admin/edit.php?post_type=forum_topic
-http://localhost/wp-admin/edit.php?post_type=forum_reply
-http://localhost/wp-admin/edit-tags.php?taxonomy=forum&post_type=forum_topic
-http://localhost/wp-admin/edit-tags.php?taxonomy=forum_tag&post_type=forum_topic
-*/
-
+	/* Add the reply menu page. */
+	add_submenu_page( 
+		"edit.php?post_type={$forum_type}", 
+		$reply_object->labels->all_items, 
+		$reply_object->labels->all_items, 
+		$reply_object->cap->edit_posts, 
+		"edit.php?post_type={$reply_type}" 
+	);
 }
-
-add_action( 'admin_enqueue_scripts', 'mb_admin_enqueue_scripts' );
-
-function mb_admin_enqueue_scripts( $hook_suffix ) {
-	global $typenow;
-
-	if ( 'edit-tags.php' === $hook_suffix && 'forum_topic' === $typenow )
-		wp_enqueue_style( 'mb-admin', trailingslashit( message_board()->dir_uri ) . 'css/admin.css' );
-}
-
-
-
-
-
-
-
-
