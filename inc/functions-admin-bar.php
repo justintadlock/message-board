d1685c8bc38487f7359651e93281a555b9343727
Justin Tadlock <justin@justintadlock.com>
2014-10-24 15:03:50 -0500

    Clean up the admin bar functionality.

--- inc/functions-admin-bar.php
+++ inc/functions-admin-bar.php
@@ -1,9 +1,18 @@
 <?php
 
+/* Hook into to change the admin bar output. */
 add_action( 'wp_before_admin_bar_render', 'mb_admin_bar' );
 
+/**
+ * Removes the ability to add a new forum topic from the admin bar.
+ *
+ * @since  1.0.0
+ * @access public
+ * @global object  $wp_admin_bar
+ * @return void
+ */
 function mb_admin_bar() {
 	global $wp_admin_bar;
 
-	$wp_admin_bar->remove_menu( 'new-forum_topic' );
+	$wp_admin_bar->remove_menu( 'new-' . mb_get_topic_post_type() );
 }
