8492d2aced8243ce5a11f762dffe908a6580d07b
Justin Tadlock <justin@justintadlock.com>
2015-01-15 17:50:14 -0600

    Complete overhaul of the custom submit/publish meta box.  Now, you can actually understand the code.

--- admin/meta-boxes.php
+++ admin/meta-boxes.php
@@ -22,22 +22,33 @@
  * @return void
  */
 function mb_submit_meta_box( $post, $args = array() ) {
-	global $action;
 
-	$avail_statuses   = array();
-	$post_type        = $post->post_type;
-	$post_type_object = get_post_type_object( $post_type );
+	$forum_type = mb_get_forum_post_type();
+	$topic_type = mb_get_topic_post_type();
+	$reply_type = mb_get_reply_post_type();
 
-	if ( mb_is_forum( $post->ID ) )
-		$avail_statuses = mb_get_forum_post_statuses();
+	$post_type     = $post->post_type;
+	$post_status   = $post->post_status;
+	$hidden_status = 'auto-draft' === $post_status ? 'draft' : $post_status;
 
-	elseif ( mb_is_topic( $post->ID ) )
-		$avail_statuses = mb_get_topic_post_statuses();
+	/* If the post is a forum. */
+	if ( $forum_type === $post_type ) {
 
-	elseif ( mb_is_reply( $post->ID ) )
-		$avail_statuses = mb_get_reply_post_statuses();
+		$allowed_stati = mb_get_forum_post_statuses();
+		$status_obj    = in_array( $post_status, $allowed_stati ) ? get_post_status_object( $post_status ) : get_post_status_object( mb_get_open_post_status() );
 
-	$can_publish = current_user_can( $post_type_object->cap->publish_posts ); ?>
+	/* If the post is a topic. */
+	} elseif ( $topic_type === $post_type ) {
+
+		$allowed_stati = mb_get_topic_post_statuses();
+		$status_obj    = in_array( $post_status, $allowed_stati ) ? get_post_status_object( $post_status ) : get_post_status_object( mb_get_open_post_status() );
+
+	/* If the post is a reply. */
+	} elseif ( $reply_type === $post_type ) {
+
+		$allowed_stati = mb_get_reply_post_statuses();
+		$status_obj    = in_array( $post_status, $allowed_stati ) ? get_post_status_object( $post_status ) : get_post_status_object( mb_get_publish_post_status() );
+	} ?>
 
 	<div class="submitbox" id="submitpost">
 
@@ -46,59 +57,55 @@ function mb_submit_meta_box( $post, $args = array() ) {
 			<div id="misc-publishing-actions">
 
 				<div class="misc-pub-section misc-pub-post-status">
-<?php $st_object = !empty( $post->post_status ) && in_array( $post->post_status, $avail_statuses ) ? get_post_status_object( $post->post_status ) : get_post_status_object( mb_get_open_post_status() ); ?>
 
-<label for="post_status"><?php printf( __( 'Status: %s', 'message-board' ), "<strong class='mb-current-status'>{$st_object->label}</strong>" ); ?></label>
+					<label for="post_status">
+						<?php printf( __( 'Status: %s', 'message-board' ), "<strong class='mb-current-status'>{$status_obj->label}</strong>" ); ?>
+					</label>
 
-<a href="#post-status-select" class="edit-post-status hide-if-no-js">
-	<span aria-hidden="true"><?php _e( 'Edit' ); ?></span> 
-	<span class="screen-reader-text"><?php _e( 'Edit status' ); ?></span>
-</a>
+					<a href="#post-status-select" class="edit-post-status hide-if-no-js">
+						<span aria-hidden="true"><?php _e( 'Edit', 'message-board' ); ?></span> 
+						<span class="screen-reader-text"><?php _e( 'Edit status', 'message-board' ); ?></span>
+					</a>
 
 					<div id="post-status-select" class="hide-if-js">
-						<input type="hidden" name="hidden_post_status" id="hidden_post_status" value="<?php echo esc_attr( ('auto-draft' == $post->post_status ) ? 'draft' : $post->post_status); ?>" />
-							<div id="post_status">
-						<?php if ( mb_is_forum( $post->ID ) ) :
-							mb_dropdown_forum_status( array( 'selected' => $post->post_status ) );
-						elseif ( mb_is_topic( $post->ID ) ) :
-							mb_dropdown_topic_status( array( 'selected' => $post->post_status ) );
-						elseif ( mb_is_reply( $post->ID ) ) :
-							mb_dropdown_reply_status( array( 'selected' => $post->post_status ) );
-						endif;
-						?>
- <a href="#post_status" class="save-post-status hide-if-no-js button"><?php _e('OK'); ?></a>
- <a href="#post_status" class="cancel-post-status hide-if-no-js button-cancel"><?php _e('Cancel'); ?></a>
-							</div><!-- #post_status -->
+
+						<input type="hidden" name="hidden_post_status" id="hidden_post_status" value="<?php echo esc_attr( $hidden_status ); ?>" />
+
+						<div id="post_status">
+
+							<?php mb_dropdown_forum_status(
+								array(
+									'selected' => $post_status,
+									'id'       => 'post_status',
+									'name'     => 'post_status'
+								)
+							); ?>
+
+							<a href="#post_status" class="save-post-status hide-if-no-js button"><?php _e( 'OK', 'message-board' ); ?></a>
+							<a href="#post_status" class="cancel-post-status hide-if-no-js button-cancel"><?php _e( 'Cancel', 'message-board' ); ?></a>
+
+						</div><!-- #post_status -->
+
 					</div><!-- #post-status-select -->
 
 				</div><!-- .misc-pub-section -->
 
-<?php
-/* translators: Publish box date format, see http://php.net/date */
-$datef = __( 'M j, Y @ G:i' );
-if ( 0 != $post->ID ) {
-	$stamp = __('Date: <b>%1$s</b>');
-	$date = date_i18n( $datef, strtotime( $post->post_date ) );
-} else {
-	$stamp = __('Publish <b>immediately</b>');
-	$date = date_i18n( $datef, strtotime( current_time('mysql') ) );
-}
+				<?php
+				/* Get the post date. */
+				$date_mysql = 0 != $post->ID ? $post->post_date : current_time( 'mysql' );
 
-?>
+				/* Translators: Publish box date format. */
+				$date_i18n = date_i18n( __( 'M j, Y @ G:i', 'message-board' ), strtotime( $date_mysql ) );
+				?>
 
-<?php if ( mb_is_topic( $post->ID ) ) : ?>
-<?php $_m_order = 0 != $post->ID ? $post->post_date : current_time( 'mysql' ); ?>
-<input type="hidden" name="menu_order" value="<?php echo esc_attr( mysql2date( 'U', $_m_order ) ); ?>" />
-<?php endif; ?>
-
-<div class="misc-pub-section curtime misc-pub-curtime">
-	<span id="timestamp">
-	<?php printf($stamp, $date); ?></span>
-</div>
+				<div class="misc-pub-section curtime misc-pub-curtime">
+					<span id="timestamp"><?php printf( __( 'Date: %s', 'message-board' ), "<strong>{$date_i18n}</strong>" ); ?></span>
+				</div><!-- .misc-pub-curtime -->
 
 				<div class="misc-pub-section">
-<i class="dashicons dashicons-admin-users"></i> <?php printf( __( 'Author: %s', 'message-board' ), '<strong>' . get_the_author_meta( 'display_name', $post->post_author ) . '</strong>' ); ?>
-				</div>
+					<i class="dashicons dashicons-admin-users"></i> 
+					<?php printf( __( 'Author: %s', 'message-board' ), '<strong>' . get_the_author_meta( 'display_name', $post->post_author ) . '</strong>' ); ?>
+				</div><!-- .misc-pub-section -->
 
 				<?php do_action( 'post_submitbox_misc_actions' ); ?>
 
@@ -113,25 +120,31 @@ if ( 0 != $post->ID ) {
 			<?php do_action( 'post_submitbox_start' ); ?>
 
 			<div id="delete-action">
-				<?php if ( current_user_can( 'delete_post', $post->ID ) ) :
-					if ( !EMPTY_TRASH_DAYS ) :
-						$delete_text = __( 'Delete Permanently', 'message-board' );
-					else :
-						$delete_text = __( 'Move to Trash', 'message-board' );
-					endif; ?>
-					<a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>"><?php echo $delete_text; ?></a>
+
+				<?php if ( current_user_can( 'delete_post', $post->ID ) ) : ?>
+					<a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>">
+						<?php !EMPTY_TRASH_DAYS ? _e( 'Delete Permanently', 'message-board' ) : _e( 'Move to Trash', 'message-board' ) ?>
+					</a>
 				<?php endif; ?>
+
 			</div><!-- #delete-action -->
 
 			<div id="publishing-action">
+
 				<span class="spinner"></span>
-				<?php if ( 0 == $post->ID || !in_array( $post->post_status, $avail_statuses ) ) : ?>
+
+				<?php if ( 0 == $post->ID || !in_array( $post_status, $allowed_stati ) ) : ?>
+
 					<input name="original_publish" type="hidden" id="original_publish" value="<?php esc_attr_e( 'Publish', 'message-board' ) ?>" />
 					<?php submit_button( __( 'Publish', 'message-board' ), 'primary button-large', 'mb-publish', false, array( 'accesskey' => 'p' ) ); ?>
+
 				<?php else : ?>
+
 					<input name="original_publish" type="hidden" id="original_publish" value="<?php esc_attr_e( 'Update', 'message-board' ) ?>" />
 					<input name="save" type="submit" class="button button-primary button-large" id="publish" accesskey="p" value="<?php esc_attr_e( 'Update', 'message-board' ) ?>" />
+
 				<?php endif; ?>
+
 			</div><!-- #publishing-action -->
 
 			<div class="clear"></div>
@@ -190,7 +203,8 @@ function mb_forum_attributes_meta_box( $post ) {
 }
 
 /**
- * Topic attributes meta box.  This handles whether the topic is sticky and the parent forum.
+ * Topic attributes meta box.  This handles whether the topic is sticky and the parent forum. It also 
+ * has the hidden input to save the proper `menu_order` field for the post.
  *
  * @since  1.0.0
  * @access public
@@ -226,8 +240,11 @@ function mb_topic_attributes_meta_box( $post ) {
 				'selected'   => !empty( $post->post_parent ) ? $post->post_parent : mb_get_default_forum_id()
 			)
 		); ?>
-	</p><?php
-}
+	</p>
+
+	<?php $order = 0 != $post->ID ? $post->post_date : current_time( 'mysql' ); ?>
+	<input type="hidden" name="menu_order" value="<?php echo esc_attr( mysql2date( 'U', $order ) ); ?>" />
+<?php }
 
 /**
  * Reply info meta box.  Displays relevant information about the reply.  This box doesn't have editable 
